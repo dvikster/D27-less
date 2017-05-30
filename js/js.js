d45ef4c0537ceae0f3d8986585a3d94b71531f6b
dvikster <dvikster@gmail.com>
2017-05-30 18:08:57 +0300

    added files

--- js/js.js
+++ js/js.js
@@ -33,21 +33,20 @@ var  genPassStrong = {
     'before': '',
     // 'after': '',
     'url': '',
-    'salt': '2;+/<-',
+    'salt': '2;b+/d<-fa1',
     'pass': function () {
         var saltNew = genPassStrong.salt;
-        // for(var i=0; i<4; i++){
-            console.log(saltNew);
-            var rand = 1 + Math.floor(Math.random() * (6 + 1 - 1));
-            console.log(rand);
-            saltNew +=saltNew.charAt(rand);
-            console.log(saltNew);
-        // }
-
+        var saltNew1 ='';
+        for(var i=0; i<4; i++){
+            var rand = Math.floor(Math.random() * 10);
+            saltNew1 +=saltNew.charAt(rand);
+            console.log(saltNew1);
+        }
+        console.log('2 - '+saltNew1);
         genPassStrong.before = $('#before').val();
         genPassStrong.after = $('#after').val();
         genPassStrong.url = $('#url').val();
-        var newUrl = genPass.url;
+        var newUrl = genPassStrong.url;
         newUrl = newUrl.replace(/http:\/\//g,'');
         newUrl = newUrl.replace(/https:\/\//g,'');
         newUrl = newUrl.replace(/www./g,'');
@@ -55,11 +54,20 @@ var  genPassStrong = {
         newUrl = newUrl.replace(/l/g, '1');
         newUrl = newUrl.replace(/i/g, 'I');
         newUrl = newUrl.replace(/s/gi, '5');
+        newUrl = newUrl.replace(/2/g, 'Z');
+        newUrl = newUrl.replace(/z/g, '2');
+        newUrl = newUrl.replace(/4/g, 'H');
+        newUrl = newUrl.replace(/h/g, '4');
         newUrl = newUrl.replace(/ /gi, '');
         var dotPos = newUrl.search(/\./);
-        newUrl = newUrl.substr(0,dotPos);
-        var newPass = genPassStrong.before+newUrl+genPassStrong.after;
-        $('.new_pass').html(newPass);
+        if(dotPos>0){newUrl = newUrl.substr(0,dotPos);}
+        var newPassStrong = genPassStrong.before+newUrl+saltNew1;
+        // var newPassStrong = genPassStrong.before+newUrl;
+        // console.log(newPassStrong.charAt(Math.floor(Math.random() * 10)));
+        // newPassStrong = newPassStrong.replace(/1/g, newPassStrong.charAt(Math.floor(Math.random() * 10)));
+        // newPassStrong = newPassStrong.replace(/5/g, newPassStrong.charAt(Math.floor(Math.random() * 10)));
+        // newPassStrong = newPassStrong.replace(/6/g, newPassStrong.charAt(Math.floor(Math.random() * 10)));
+        $('.new_pass').html(newPassStrong);
     }};
 genPassStrong.pass();
 
