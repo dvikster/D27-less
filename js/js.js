2d69937d227fc88c15eb5e790dc6aa22702b92ce
dvikster <dvikster@gmail.com>
2017-05-30 21:05:28 +0300

    upd

--- js/js.js
+++ js/js.js
@@ -5,71 +5,86 @@ $(document).ready(function () {
 var before, after, url;
 
 var  genPass = {
-    'before': '',
-    'after': '',
+    'before': '738',
+    'after': 'dtT',
     'url': '',
     'pass': function () {
-        genPass.before = $('#before').val();
-        genPass.after = $('#after').val();
+        $('#url').removeClass('empty');
+        $('.error').removeClass('visible');
         genPass.url = $('#url').val();
-        var newUrl = genPass.url;
-        newUrl = newUrl.replace(/http:\/\//g,'');
-        newUrl = newUrl.replace(/https:\/\//g,'');
-        newUrl = newUrl.replace(/www./g,'');
-        newUrl = newUrl.replace(/o/gi, '0');
-        newUrl = newUrl.replace(/l/g, '1');
-        newUrl = newUrl.replace(/i/g, 'I');
-        newUrl = newUrl.replace(/s/gi, '5');
-        newUrl = newUrl.replace(/ /gi, '');
-        var dotPos = newUrl.search(/\./);
-        if(dotPos>0){newUrl = newUrl.substr(0,dotPos);}
-        var newPass = genPass.before + newUrl + genPass.after;
-        $('.new_pass').html(newPass);
+        if( genPass.url != ''){
+            //genPass.before = $('#before').val();
+            //genPass.after = $('#after').val();
+            var newUrl = genPass.url;
+            newUrl = newUrl.replace(/http:\/\//g,'');
+            newUrl = newUrl.replace(/https:\/\//g,'');
+            newUrl = newUrl.replace(/www./g,'');
+            newUrl = newUrl.replace(/o/gi, '0');
+            newUrl = newUrl.replace(/l/g, '1');
+            newUrl = newUrl.replace(/i/g, 'I');
+            newUrl = newUrl.replace(/s/gi, '5');
+            newUrl = newUrl.replace(/ /gi, '');
+            var dotPos = newUrl.search(/\./);
+            if(dotPos>0){newUrl = newUrl.substr(0,dotPos);}
+            var newPass = genPass.before + newUrl + genPass.after;
+            $('.new_pass').html(newPass);
+        }
+        else{
+            $('#url').addClass('empty');
+            $('.error').addClass('visible');
+        }
     }};
-    genPass.pass();
+    //genPass.pass();
 
 
 var  genPassStrong = {
-    'before': '',
+    'before': '738,',
     // 'after': '',
     'url': '',
     'salt': '2;b+/d<-fa1',
     'pass': function () {
-        var saltNew = genPassStrong.salt;
-        var saltNew1 ='';
-        for(var i=0; i<4; i++){
-            var rand = Math.floor(Math.random() * 10);
-            saltNew1 +=saltNew.charAt(rand);
-            console.log(saltNew1);
-        }
-        console.log('2 - '+saltNew1);
-        genPassStrong.before = $('#before').val();
-        genPassStrong.after = $('#after').val();
+        $('#url').removeClass('empty');
+        $('.error').removeClass('visible');
         genPassStrong.url = $('#url').val();
-        var newUrl = genPassStrong.url;
-        newUrl = newUrl.replace(/http:\/\//g,'');
-        newUrl = newUrl.replace(/https:\/\//g,'');
-        newUrl = newUrl.replace(/www./g,'');
-        newUrl = newUrl.replace(/o/gi, '0');
-        newUrl = newUrl.replace(/l/g, '1');
-        newUrl = newUrl.replace(/i/g, 'I');
-        newUrl = newUrl.replace(/s/gi, '5');
-        newUrl = newUrl.replace(/2/g, 'Z');
-        newUrl = newUrl.replace(/z/g, '2');
-        newUrl = newUrl.replace(/4/g, 'H');
-        newUrl = newUrl.replace(/h/g, '4');
-        newUrl = newUrl.replace(/ /gi, '');
-        var dotPos = newUrl.search(/\./);
-        if(dotPos>0){newUrl = newUrl.substr(0,dotPos);}
-        var newPassStrong = genPassStrong.before+newUrl+saltNew1;
-        // var newPassStrong = genPassStrong.before+newUrl;
-        // console.log(newPassStrong.charAt(Math.floor(Math.random() * 10)));
-        // newPassStrong = newPassStrong.replace(/1/g, newPassStrong.charAt(Math.floor(Math.random() * 10)));
-        // newPassStrong = newPassStrong.replace(/5/g, newPassStrong.charAt(Math.floor(Math.random() * 10)));
-        // newPassStrong = newPassStrong.replace(/6/g, newPassStrong.charAt(Math.floor(Math.random() * 10)));
-        $('.new_pass').html(newPassStrong);
+
+        if(genPassStrong.url != ''){
+            var saltNew = genPassStrong.salt;
+            var saltNew1 ='';
+            for(var i=0; i<4; i++){
+                var rand = Math.floor(Math.random() * 10);
+                saltNew1 +=saltNew.charAt(rand);
+                console.log(saltNew1);
+            }
+            console.log('2 - '+saltNew1);
+            //genPassStrong.before = $('#before').val();
+            //genPassStrong.url = $('#url').val();
+            var newUrl = genPassStrong.url;
+            newUrl = newUrl.replace(/http:\/\//g,'');
+            newUrl = newUrl.replace(/https:\/\//g,'');
+            newUrl = newUrl.replace(/www./g,'');
+            newUrl = newUrl.replace(/o/gi, '0');
+            newUrl = newUrl.replace(/l/g, '1');
+            newUrl = newUrl.replace(/i/g, 'I');
+            newUrl = newUrl.replace(/s/gi, '5');
+            newUrl = newUrl.replace(/2/g, 'Z');
+            newUrl = newUrl.replace(/z/g, '2');
+            newUrl = newUrl.replace(/4/g, 'H');
+            newUrl = newUrl.replace(/h/g, '4');
+            newUrl = newUrl.replace(/ /gi, '');
+            var dotPos = newUrl.search(/\./);
+            if(dotPos>0){newUrl = newUrl.substr(0,dotPos);}
+            newUrl = newUrl.replace(newUrl[3], saltNew.charAt(0));
+            newUrl = newUrl.replace(newUrl[5], saltNew.charAt(1));
+            newUrl = newUrl.replace(newUrl[7], saltNew.charAt(2));
+            var newPassStrong = genPassStrong.before+newUrl+saltNew1;
+            $('.new_pass').html(newPassStrong);
+        }
+        else{
+            $('#url').addClass('empty');
+            $('.error').addClass('visible');
+        }
     }};
-genPassStrong.pass();
+//genPassStrong.pass();
 
 // function getDataStrong() {
 //     before = $('#before').val();
